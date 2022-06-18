cb4cdc008a400749123dc796a6c5a404f00609df
igo <igoranatol870@gmail.com>
2022-06-18 09:16:54 +0300

    sdfsdf

---
+++ js/bu_or_del.js
@@ -0,0 +1,97 @@
+function bu_or_sal () {
+    try {
+        if(confirm("Вы можете покупать, или продавать акци")) {
+            if(confirm("Но перед тем как покупать, надо сначала зарегестрировать свой i")) {
+                if(confirm("Готовы зарегестриро")) {
+                    if(confirm("Введите своё им")) {
+                        var us_use_i1 = {
+                            us_use_i1_usi1: {
+                                us_use_i1_usi1_nm1:"igo",
+                                us_use_i1_usi1_sr1:"romane",
+                                us_use_i1_usi1_ag1:"19"
+                            }
+                        };
+                        if(us_use_i1.hasOwnProperty("us_use_i1_usi1.us_use_i1_usi1_nm1") == true && us_use_i1.hasOwnProperty("us_use_i1_usi1.us_use_i1_usi1_sr1") == true) {
+                            var us_use_nm1;
+                            var us_use_sr1;
+                            var us_use_ag1;
+                            us_use_nm1 = prompt("Введите своё им");
+                            if(us_use_nm1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_nm1 && us_use_nm1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_sr1) throw new Error("У вас ошибка, поле не может быть пуст");
+                            else if(us_use_nm1 == us_use_i1.us_use_i1_usi1.us_use_i1_usi1_nm1 && us_use_nm1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_sr1) {
+                                us_use_sr1 = prompt("Введите свою фамил");
+                                if(us_use_sr1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_sr1 && us_use_sr1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_ag1) throw new Error("У вас ошибка, поле не может быть пуст");
+                                else if(us_use_sr1 == us_use_i1.us_use_i1_usi1.us_use_i1_usi1_sr1 && us_use_sr1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_ag1) {
+                                    us_use_ag1 = prompt("Введите свой возра");
+                                    if(us_use_ag1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_ag1 && us_use_nm1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_nm1) throw new Error("У вас ошибка, поле не может быть пуст");
+                                    else if(us_use_ag1 == us_use_i1.us_use_i1_usi1.us_use_i1_usi1_ag1 && us_use_sr1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_nm1) {
+                                        if(confirm("Вы вошл")) {
+                                            if(confirm("Здравтсву" + " " + us_use_i1.us_use_i1_usi1.us_use_i1_usi1_nm1 + " " + us_use_i1.us_use_i1_usi1.us_use_i1_usi1_sr1)) {
+                                                if(confirm("Можете посмотреть ваш бала")) {
+                                                    if(confirm("Это платная функци")) {
+                                                        if(confirm("Посмотерть бала")) {
+                                                            Object.defineProperty(us_use_i1, "us_use_i1_usi1_pri", {value:"1599", writable:true, enumerable:true, configurable:true});
+                                                            if(us_use_i1.hasOwnProperty("us_use_i1_usi1_pri") == true && us_use_i1.us_use_i1_usi1_pri == "1599") {
+                                                                if(confirm("Можете покупать или продавать акц")) {
+                                                                    if(confirm("Можете посмотреть список акц")) {
+                                                                        
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    catch(e) {
+        if(confirm("У вас нету своего i")) {
+            if(confirm("Можем прямо сейчас его зарегестрирова")) {
+                if(confirm("Введите своё им")) {
+                    var us_usi1_nm1;
+                    var us_usi1_sr1;
+                    var us_usi1_ag1;
+                    us_usi1_nm1 = prompt("Введите своё им");
+                    if(us_usi1_nm1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_nm1 && us_usi1_nm1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_sr1) {
+                        us_usi1_sr1 = prompt("Введите свою фамил");
+                        if(us_usi1_sr1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_sr1 && us_usi1_sr1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_ag1) {
+                            us_usi1_ag1 = prompt("Введите свой возра");
+                            if(us_usi1_ag1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_ag1 && us_usi1_ag1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_nm1) {
+                                if(confirm("Зарегестрировать i")) {
+                                    if(confirm("За это снимется пла")) {
+                                        if(confirm("Пежде чем покупать или продавать, надо сначала оплатить дол")) {
+                                            Object.defineProperty(us_use_i1, "us_use1_desc1", {value:"1377", writable:true, enumerable:true, configurable:true});
+                                            if(us_use_i1.hasOwnProperty("us_use1_desc1") == true && us_use_i1.hasOwnProperty("us_use_i1_usi1.us_use_i1_usi1_nm1") == true) {
+                                                Object.defineProperty(us_use_i1, "us_use_i1_nm1", {value:us_usi1_nm1, writable:true, enumerable:true, configurable:true});
+                                                if(us_use_i1.hasOwnProperty("us_use_i1_nm1") == true && us_use_i1.us_use_i1_nm1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_nm1) {
+                                                    Object.defineProperty(us_use_i1, "us_use_i1_sr1", {value:us_usi1_sr1, writable:true, enumerable:true, configurable:true});
+                                                    if(us_use_i1.hasOwnProperty("us_use_i1_sr1") == true && us_use_i1.us_use_i1_sr1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_sr1) {
+                                                        Object.defineProperty(us_use_i1, "us_use_i1_ag1", {value:us_usi1_ag1, writable:true, enumerable:true, configurable:true});
+                                                        if(us_use_i1.hasOwnProperty("us_use_i1_ag1") == true && us_use_i1.us_use_i1_ag1 != us_use_i1.us_use_i1_usi1.us_use_i1_usi1_ag1) {
+                                                            if(confirm("Ваш id был зарегестрирова")) {
+                                                                if(confirm("Посмотреть профи")) {
+                                                                    
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
